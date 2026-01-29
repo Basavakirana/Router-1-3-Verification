35e3e34da826e78bfe3d0c60a3601aca03198c03
A M Basavakiran <91108699+Basavakirana@users.noreply.github.com>
2026-01-29 11:03:38 +0530

    Add base_test class with environment configuration

--- tb/env_config.sv
+++ tb/env_config.sv
@@ -38,5 +38,41 @@ class env_config extends uvm_object;
     super.new(name);
   endfunction
 
-endclass   */
+endclass   
+class base_test extends uvm_test;
+  `uvm_component_utils(base_test)
+
+  env_config env_cfg;
+
+  function new(string name="base_test", uvm_component parent=null);
+    super.new(name,parent);
+  endfunction
+
+  function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+
+    env_cfg = env_config::type_id::create("env_cfg");
+
+    // Set topology HERE
+    env_cfg.no_of_source      = 1;
+    env_cfg.no_of_destination = 3;
+    env_cfg.has_scoreboard    = 1;
+    env_cfg.has_vsequencer    = 1;
+    env_cfg.has_sagent        = 1;
+    env_cfg.has_dagent        = 1;
+
+    // Create agent configs based on count
+    env_cfg.s_configh = new[env_cfg.no_of_source];
+    foreach (env_cfg.s_configh[i])
+      env_cfg.s_configh[i] = source_agt_config::type_id::create($sformatf("s_cfg[%0d]", i));
+
+    env_cfg.d_configh = new[env_cfg.no_of_destination];
+    foreach (env_cfg.d_configh[i])
+      env_cfg.d_configh[i] = dest_agt_config::type_id::create($sformatf("d_cfg[%0d]", i));
+
+    // Put into config DB
+    uvm_config_db#(env_config)::set(this, "*", "env_config", env_cfg);
+  endfunction
+
+endclass */
 
